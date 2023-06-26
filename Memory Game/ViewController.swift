98815b9692775e2883a2d7fe82f429c8813751da
Diego Flores <diegofla0731@gmail.com>
2023-06-26 17:02:48 -0600

    Initial Commit

---
+++ Memory Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory Game
+//
+//  Created by Didami on 26/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .red
+    }
+}
